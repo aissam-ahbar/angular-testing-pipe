e43d1e8bdd787cb1b65592c35739ab172d133bda
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-12-06 18:39:24 +0100

    1 file modified

--- src/app/pipe/custompipe.pipe.ts
+++ src/app/pipe/custompipe.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: 'customPipe', pure: true })
+@Pipe({ name: 'custompipe', pure: true })
 export class CustomPipe implements PipeTransform {
   transform(input: string): string {
     return input.length === 0 ? '' : input.toUpperCase();
